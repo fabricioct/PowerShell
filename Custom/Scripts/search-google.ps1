14bd7cbd7af9d357f628284e3eec00a3d233c12d
Fabricio C.T <fabriciocotta@gmail.com>
2023-05-25 10:10:38 -0300

    "Add Scripts"

---
+++ Custom/Scripts/search-google.ps1
@@ -0,0 +1,17 @@
+﻿<#
+
+#>
+
+param([string]$SearchMessage = "")
+
+try {
+	
+   if ($SearchMessage -eq "") { $SearchMessage = read-host "Enter the message to search" }
+	
+	& "$PSScriptRoot/../../Scripts/open-default-browser.ps1" "https://www.google.com/search?q=$SearchMessage"
+
+	exit 0 # success
+} catch {
+	"⚠️ Error in line $($_.InvocationInfo.ScriptLineNumber): $($Error[0])"
+	exit 1
+}
